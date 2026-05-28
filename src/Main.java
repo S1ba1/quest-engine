fcfeadd55902bd523fd76291dc0abc8707607c2a
Sibai Shouk <sibaishouk@gmail.com>
2026-05-28 13:52:56 +0200

    feat: add console UI menu flow

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
-public class Main {
+import model.*;
+import service.*;
+import ui.*;
 
+public class Main {
+    public static void main(String[] args) {
+        QuestService questService = new QuestService();
+        // RewardService rewardService = new RewardService;
+
+        ConsoleUI ui = new ConsoleUI(questService);
+        ui.start();
+    }
 }
---
+++ src/ui/ConsoleUI.java
@@ -0,0 +1,119 @@
+package ui;
+import model.*;
+import service.QuestService;
+import java.util.*;
+
+
+public class ConsoleUI {
+    private final QuestService userQuestService;
+    // private final Player userPlayer;
+
+    private final Scanner userIn = new Scanner(System.in);
+
+    public ConsoleUI(QuestService userQuestService) {
+        this.userQuestService = userQuestService;
+        // this.userPlayer = userPlayer;
+    }
+
+    public void start() {
+        while(true) {
+            showMenu();
+            System.out.println("Please pick by using a valid number: ");
+            int choice = Integer.parseInt(userIn.nextLine());
+            switch (choice) {
+                case 1:
+                    handleAddQuest();
+                    break;
+                case 2:
+                    handleViewActiveQuests();
+                    break;
+                case 3:
+                    handleCompleteQuest();
+                    break;
+                case 4:
+                    handleFailQuest();
+                    break;
+                case 5:
+                    handleViewAllQuests();
+                    break;
+                case 6:
+                    return;
+                
+                default:
+                    System.out.println("invalid number! Please enter a valid number!");
+                    break;
+            }
+        }
+    }
+
+    private void showMenu() {
+        System.out.println(
+            "1. Add Quest\n" + 
+            "2. View Active Quests\n" + 
+            "3. Complete Quest\n" + 
+            "4. Fail Quest\n" + 
+            "5. View All Quests\n" + 
+            "6. Exit"
+        );
+    }
+
+    private void handleAddQuest() {
+        System.out.println("Title: ");
+        String qTitle = userIn.nextLine();
+        System.out.println("description: ");
+        String qDescription = userIn.nextLine();
+        userQuestService.addQuest(qTitle, qDescription, readSkill(), readDifficulty());
+        System.out.println(qTitle + " is now a quest!");
+    }
+
+    private Skill readSkill() {
+        while (true) {
+            System.out.println("Skill: ");
+            try {
+                String skillString = userIn.nextLine()
+                        .trim()
+                        .toUpperCase();
+                return Skill.valueOf(skillString);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid input. Please try again!");
+            }
+        }
+    }
+
+    private Difficulty readDifficulty() {
+        while (true) {
+            System.out.println("Difficulty: ");
+            try {
+                String difficultyString = userIn.nextLine()
+                        .trim()
+                        .toUpperCase();
+                return Difficulty.valueOf(difficultyString);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid input. Please try again!");
+            }
+        }
+    }
+
+    private void handleViewActiveQuests() {
+        System.out.println(userQuestService.getActiveQuests());
+    }
+
+    private void handleCompleteQuest() {
+        System.out.println("Quest-ID: ");
+        int qId = Integer.parseInt(userIn.nextLine());
+        userQuestService.completeQuest(qId);
+        System.out.println("Quest with ID " + qId +" is now marked as completed!");
+    }
+
+    private void handleFailQuest() {
+        System.out.println("Quest-ID: ");
+        int qId = Integer.parseInt(userIn.nextLine());
+        userQuestService.failQuest(qId);
+        System.out.println("Quest with ID " + qId +" is now marked as failed!");
+    }
+
+    private void handleViewAllQuests() {
+        System.out.println(userQuestService.getAllQuests()); 
+    }
+
+}
