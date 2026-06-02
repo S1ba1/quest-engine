43abb85519af25d129c27655581d7ee0396e76a6
Sibai Shouk <sibaishouk@gmail.com>
2026-06-02 22:19:33 +0200

    feat: implement reward service

--- src/service/RewardService.java
+++ src/service/RewardService.java
@@ -1,5 +1,28 @@
 package service;
 
+import model.*;
+
+/*
+good future responsibilities:
+- reward player with quest XP
+- detect level ups
+- prevent rewarding a quest twice
+- return a reward result/message
+- handle bonus XP later
+- handle streak bonuses later
+- unlock achievements later
+*/
+
 public class RewardService {
 
+    private final Player player;
+
+    public RewardService(Player player) {
+        this.player = player;
+    }
+
+    public void rewardPlayer(Quest quest) {
+
+        player.rewardXP(quest.getXpReward());
+    }
 }
