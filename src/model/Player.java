6dafec94ced61f27378bfdfc8eb2dc01153c0651
Sibai Shouk <sibaishouk@gmail.com>
2026-06-02 22:18:00 +0200

    feat: add player model with XP progression

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,14 +1,60 @@
 package model;
 
 public class Player {
-    //simple implementation
+    // simple implementation
     private final String name;
+    private long level;
+    private long currentXP;
+    private long totalXP;
 
-    Player(String name) {
+    private Player(String name, long level, long currentXP, long totalXP) {
         this.name = name;
+        this.level = level;
+        this.currentXP = currentXP;
+        this.totalXP = totalXP;
+    }
+
+    public static Player create(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("A Player must have a name, it can't be blank");
+        }
+        return new Player(name, 1, 0, 0);
     }
 
     public String getName() {
         return name;
-    } 
+    }
+
+    public long getLevel() {
+        return level;
+    }
+
+    public long getcurrentXP() {
+        return currentXP;
+    }
+
+    public long getTotalXP() {
+        return totalXP;
+    }
+
+    private void levelUp() {
+        level++;
+        currentXP = 0;
+    }
+
+    public long getLevelThreshhold() {
+        return level * 100;
+    }
+
+    public void rewardXP(long xp) {
+        totalXP += xp;
+        if (currentXP + xp > getLevelThreshhold()) {
+            long remainingXp = currentXP + xp - getLevelThreshhold();
+            levelUp();
+            rewardXP(remainingXp);
+        } else {
+            currentXP += xp;
+        }
+    }
+
 }
